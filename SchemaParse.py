0759891e0715efd0e2b10360fcf09489a184aefe
= <benjamin.schwab@duke.edu>
2013-12-08 01:26:10 -0500

    Python config parse JS graph builder

---
+++ SchemaParse.py
@@ -0,0 +1,104 @@
+import sqlite3
+import json
+
+
+#TODO: read a database - parse out the structure return a config.json file
+
+def parseDatabase():
+   conn = sqlite3.connect('drinkers.db')
+   conn.row_factory = sqlite3.Row
+   c = conn.cursor();
+   tables = list()
+
+   #c.execute("SELECT * FROM BEER, LIKES, DRINKER WHERE DRINKER.name = 'Ben';")
+   #row = c.fetchOne()
+
+   for row in c.execute("SELECT * FROM sqlite_master WHERE type='table';"):
+      tables.append(Table(row[1]))
+   myDatabase = Database(tables)
+
+   for table in tables:
+      for row in c.execute("PRAGMA table_info("+table.name+");"):
+         col = Column(row[1])
+         table.columns.append(col)
+
+   #TODO: Clean up variables names and logic
+   for row in c.execute("SELECT * FROM sqlite_master WHERE type='table';"):
+         fTable = row[1]
+         fromTable = myDatabase.getTable(fTable)
+         createStatement = row[4]
+         stats = createStatement.split(",")
+         for s in stats:
+            if not "REFERENCES" in s: continue
+            ref = s.split("REFERENCES")
+            q = ref[0].split("(")
+            l = 0
+            ind = 1
+            if q[0].split(" ")[0] == "CREATE":
+               l = 1
+               ind = 0
+            fColumn = q[l].split(" ")[ind]
+            print(fColumn +" references")
+            fromColumn = fromTable.getColumn(fColumn)
+            p = ref[1].split("(")
+            table = p[0].strip()
+            oColumn = p[1].replace(")", "").strip()
+            otherTable = myDatabase.getTable(table)
+            otherColumn = otherTable.getColumn(oColumn)
+            fromColumn.references = otherTable.name+"."+otherColumn.name
+
+            #print "Table: "+table+", Column: "+column
+   config = open('config.json', 'w')
+   j = json.dumps(myDatabase, default=lambda o: o.__dict__)
+   print >> config, j
+   config.close();
+
+def CreateConfigJson():
+   return
+
+class Database:
+   tables = []
+   def __init__(self, tables):
+      self.tables = tables
+
+   def getTable(self, tableName):
+      #print "Looking for "+tableName
+      for t in self.tables:
+         if t.name == tableName:
+            return t
+      return None
+
+class Column:
+    """A simple example class"""
+    name = ""
+    mapsTo = []
+    reference = None
+    expose = True
+    def __init__(self, columnName):
+      self.name = columnName
+      self.references = ""
+      self.mapsTo = []
+      self.expose = True
+
+class Table:
+   """A simple example class"""
+   columns = []
+   name = ""
+   mapsTo = []
+   expose = True
+   def __init__(self, tableName):
+        self.name = tableName
+        self.mapsTo = []
+        self.columns = []
+        self.expose = True;
+   def getReferences(self):
+      return
+   def getColumn(self, columnName):
+      for c in self.columns:
+         if c.name == columnName:
+            return c
+      return None
+
+class References:
+   def f(self):
+      return 'hello world'
--- test.js
+++ test.js
@@ -1,401 +1,138 @@
-var searchbar = $(".expose_search_bar");
-var validwords = ["Cat", "Dog", "Mouse"];
-var validwordstwo =["Hippo", "Rat", "Horse"];
-resetPromptField();
+jsonString = '{"tables": [{"mapsTo": [], "expose": true, "name": "Bar", "columns": [{"mapsTo": "", "expose": true, "references": "", "name": "name"}, {"mapsTo": "", "expose": true, "references": "", "name": "address"}]}, {"mapsTo": [], "expose": true, "name": "Beer", "columns": [{"mapsTo": "", "expose": true, "references": "", "name": "name"}, {"mapsTo": "", "expose": true, "references": "", "name": "brewer"}]}, {"mapsTo": [], "expose": true, "name": "Drinker", "columns": [{"mapsTo": "", "expose": true, "references": "", "name": "name"}, {"mapsTo": "", "expose": true, "references": "", "name": "address"}]}, {"mapsTo": [], "expose": true, "name": "Frequents", "columns": [{"mapsTo": "", "expose": true, "references": "Drinker.name", "name": "drinker"}, {"mapsTo": "", "expose": true, "references": "Bar.name", "name": "bar"}, {"mapsTo": "", "expose": true, "references": "", "name": "times_a_week"}]}, {"mapsTo": [], "expose": true, "name": "Likes", "columns": [{"mapsTo": "", "expose": true, "references": "Drinker.name", "name": "drinker"}, {"mapsTo": "", "expose": true, "references": "Beer.name", "name": "beer"}]}, {"mapsTo": [], "expose": true, "name": "Serves", "columns": [{"mapsTo": "", "expose": true, "references": "Bar.name", "name": "bar"}, {"mapsTo": "", "expose": true, "references": "Beer.name", "name": "beer"}, {"mapsTo": "", "expose": true, "references": "", "name": "price"}]}]}';
 
-function invalidateSearchField(){
-   //show red x
-}
+//replace above with
+var myDatabaseConfig = eval('(' + jsonString+ ')');
 
-searchbar.keyup(function(e){
-      var phrase = $(this).val();
-      var words = phrase.split(" ");
-      for (var i = 0; i < words.length; i++){
-         words[i] = words[i].trim();
-      }
+var SQLBuilder = {
 
-      var sentence = words.slice(1,words.length).join(" ");
-      //var
-      //console.log(sentence);
-      //words.forEach(function logArrayElements(element, index, array) {console.log("a[" + index + "] = " + element);});
-      if(validatePhrase(phrase.trim()) ){
-         if(e.keyCode==32&&isExact(phrase)){ //space
-            console.log("Setting new phrase");
-            stateKeeper.setnewphrase(phrase);
-            setPromptSet(stateKeeper.buildValidPhrases());
-            setSQL();
-         }
-         else{
-            console.log("This phrase is not safe.");
-         }
-         console.log(e.keyCode);
-      }
-      else{
-         console.log("failed validation");
-      }
-      setPromptSet(validPrompts);
- });
+    buildSQLStatement: function(parseTree){
 
-function isExact(phrase){
-   phrase = phrase.trim();
-   console.log("P:"+phrase+"!");
-   for (var i=0;i<validPrompts.length;i++)
-   {
-      console.log("VP:"+validPrompts[i]+"!");
-      if(validPrompts[i].trim() === phrase ){
-         //console.log("Exact match!");
-         return true;
-      }
-   }
-}
+    },
+    extractCurrentNode: function(parseTree){
 
-function setSQL(){
-   var statement = stateKeeper.buildsql();
-   $('.sql_query_display').text(statement);
-}
+    },
+    isValidStatement: function(parseTree){
 
+    },
+    joinTables: function(tableList){
 
-function validatePhrase(phrase)
-{
-   var valid = false;
-   for (var i=0;i<validPrompts.length;i++)
-   {
-      console.log(phrase);
-      console.log(validPrompts[i]);
-      //console.log(validPrompts[i].indexOf(phrase));
-      if(validPrompts[i].indexOf(phrase)===0){
-          console.log("valid");
-          valid = true;
-      }
-      else{
-         validPrompts.splice(i,1);
-         i--;
-      }
-   }
-   //console.log("invalid");
-   return valid;
-}
-var validPrompts = [];
-function setPromptSet(prompts){
-   $('.expose_search_table').children().remove();
-    prompts.forEach(addPromptField);
-    validPrompts = prompts;
-}
-
-
- function addPromptField(text){
-   //console.log("Appendig!");
-   $('.expose_search_table').append('<tr class="expose_search_option"><td>'+text+'</td></tr>');
- }
-
- function resetPromptField(){
-      searchbar.val("Find: ");
- }
-//{
-
-//}
+    },
 
 
 
-//Methods for database graph
+};
 
-function StateKeeper(nouns, verbs){
-   this.nouns = nouns;
-   this.verbs = verbs;
-   this.nounmap = {};
-   this.verbmap = {};
-   for(var i = 0; i<nouns.length; i++){
-      this.nounmap[nouns[i].displayname] = nouns[i];
-   }
-   for(i = 0; i<verbs.length; i++){
-      this.verbmap[verbs[i].displayname] = verbs[i];
-   }
+var NameMapper = {
 
-   this.currentPhrase = "Find:";
-   this.phrasepieces = []; //maybe use for deletion
-   this.cur_state_name = "";
-   this.activenode = null;
-   this.activeproperty = null;
+};
 
-   this.tables = [];
-   this.select = null;
-   this.where = [];
-   this.partialfilter = "";
+var StateKeeper = {
 
-   this.getValidNouns = function(){
-      var valNouns = [];
-      this.nouns.forEach( function(noun){valNouns.push(noun.displayname); console.log(noun.displayname); });
-      return valNouns;
-   };
-   this.buildValidPhrases = function()
-   {
-      var build = function(noun){
-            valPhrases.push(this.currentPhrase.trim().concat(" ", noun.displayname));
-            console.log(noun.displayname);
-         };
-          var buildProp = function(prop){
-            valPhrases.push(this.currentPhrase.trim().concat(" ", prop));
-            //console.log(noun.displayname);
-         };
-      var valPhrases = [];
-      if(this.activenode === null){
-         console.log(this.nouns.length);
-         for(var i = 0; i<this.nouns.length; i++){
-            var curNoun =  this.nouns[i];
-            build.call(this, curNoun);
-         }
-      }
-      else if(this.activenode instanceof Node){
-
-         if(this.activeproperty!==null){
-            for(var l = 0; l<this.activeproperty.propertyValues.length; l++){
-               console.log("!!!"+this.activeproperty.propertyValues[l]);
-               buildProp.call(this,this.activeproperty.propertyValues[l]);
+     getTableReferenceSet: function(table){
+            var refCol = [];
+            for(var j = 0; j<table.columns.length; j++){
+                var col = table.columns[j];
+                if(col.references!==""){
+                    var ref = col.references;
+                    refCol.push(StateKeeper.getRefArray(table, col));
+                }
             }
-            this.activeproperty = null;
+        return refCol;
+    },
+    getReferenceSet: function(){
+          var refCol = [];
+          for(var i = 0; i<myDatabaseConfig.tables.length; i++){
+            var table = myDatabaseConfig.tables[i];
+            var refs = StateKeeper.getTableReferenceSet(table);
+            refCol = refCol.concat(refs);
+            //console.log(refCol);
+        }
+        //console.log(refCol);
+        return refCol;
+    },
+    isColRef: function(table, column){
+        var refSet = StateKeeper.getReferenceSet();
+        for(var i = 0; i<refSet; i++){
+            var ref = refSet[i];
 
-         }
-         else{
-
-            console.log(this.activenode.displayname);
-            transitions = this.activenode.getAllTransitions();
-            transitions.forEach(function logArrayElements(element, index, array) {console.log("a[" + index + "] = " + element.displayname);});
-             for(var k = 0; k<transitions.length; k++){
-               var curTransition =  transitions[k];
-               build.call(this, curTransition);
+            if(table === refTable && column == refCol){
+                return true;
             }
-         }
-      }
+        }
+        return false;
 
-      return valPhrases;
-   };
-
-   this.setnewphrase = function(newphrase){
-      if(newphrase.length<this.currentPhrase.length){
-         this.currentPhrase = newphrase;
-      }
-      else if(newphrase.indexOf(this.currentPhrase)===-1){
-         this.currentPhrase = newphrase;
-      }
-      else{
-         var index = newphrase.indexOf(this.currentPhrase);
-         this.cur_state_name = newphrase.substring(this.currentPhrase.length, newphrase.length);
-         this.cur_state_name = this.cur_state_name.replace(/^\s+|\s+$/g,''); //need to set equal?
-         this.currentPhrase = newphrase;
-         this.movestate(this.cur_state_name);
-      }
-   };
-
-   this.movestate = function(state_name){
-      console.log("Your statename:" + state_name);
-        if(this.partialfilter.length!==0){
-            this.partialfilter+=" " +state_name;
-            this.where.push(this.partialfilter);
-            this.partialfileter = "";
-            console.log("I created a filter");
-         }
-         if(this.activenode === null){
-            if(state_name in this.nounmap){
-               this.activenode = this.nounmap[state_name];
-               console.log("We have active node!"+this.nounmap[state_name]);
-               this.select = this.activenode;
-               this.tables.push(this.activenode.name);
+    },
+    getRefArray : function(table, refCol){
+        var refD = refCol.references.split(".");
+        if (refD.length<2){
+            return null;
+        }
+        var fromTable = table.name;
+        var refTable = refD[0];
+        var refColumn = refD[1];
+        return [fromTable, refTable, refColumn];
+    },
+     getNouns : function(){
+        nouns = [];
+        for(var i = 0; i<myDatabaseConfig.tables.length; i++){
+            var table = myDatabaseConfig.tables[i];
+            var name = table.name;
+            if(table.mapsTo.length!==0){
+                name = table.mapsTo;
             }
-            else{
-               console.log("Houston, we have a problem.");
+            nouns.push(name);
+        }
+        return nouns;
+     },
+     getValidStateTransfers : function(tableName){
+        var table = StateKeeper.getTable(tableName);
+        if(table!==null){
+            var refSet = StateKeeper.getTableReferenceSet(table);
+            var allRefSet = StateKeeper.getReferenceSet(table);
+            for(i = 0; i<allRefSet.length; i++){
+                var refArray = allRefSet[i];
+                if(refArray[1]===tableName){
+                    refSet.push(refArray);
+                }
             }
-         }
-         else{
-            var trans = this.activenode.getAllTransitions();
-            var element;
-            for(var j = 0; j<trans.length; j++){
-               if(trans[j].displayname ===state_name ){
-                  element = trans[j];
-                  console.log("WE FOUND IT!");
-               }
+            return refSet;
+        }
+     },
+     getTable: function(tableName){
+        for(var i = 0; i<myDatabaseConfig.tables.length; i++){
+            if(!table.expose) continue;
+             var table = myDatabaseConfig.tables[i];
+             if(table.name === tableName||contains(table.mapsTo,tableName)){
+                return table;
+             }
+        }
+        return null;
+     },
+     getFilterPromptsForTable: function(tableName){
+        var table = StateKeeper.getTable(tableName);
+        var prompts = [];
+        for(var i =0; i<table.columns.length; i++){
+            var curCol = table.columns[i];
+            if(!curCol.expose){
+                continue;
             }
-            //assert(element!==null);
-            //console.log("STATE: "+state_name+element.propertyValues[0]);
-            if(element instanceof Edge){
-               console.log("EDGE!");
-               this.activenode = element.toNode;
-               this.tables.push(this.activenode.name);
-            }
-            else if(element instanceof Property){
-               console.log("PROPERTY!");
-               this.activeproperty =  element;
-               this.partialfilter = this.activenode.name +"."+ this.activeproperty.name + "=";
-               //console.log(elemenent.propertValues[0]);
-            }
-         }
-   };
+            if(curCol.mapsTo.length!==0)
+                prompts = prompts.concat(curCol.mapsTo);
+            else
+                prompts.push(curCol.name);
+        }
+        return prompts;
 
-   this.buildsql = function(){
-      var select = "";
-      for(var i = 0; i<this.select.properties.length; i++){
-         select +=this.select.name +"." +this.select.properties[i].name+" ";
-      }
-      var from = this.tables.join(", ");
-      var where = this.where.join(" ");
-      var statement = "Select "+ select + " from "+ from;
-      if(where.length!==0){
-         statement = statement + " where " + where;
-      }
-      return statement;
-   };
+     }
 
+};
+
+function contains(a, obj) {
+    var i = a.length;
+    while (i--) {
+       if (a[i] === obj) {
+           return true;
+       }
+    }
+    return false;
 }
-
-/*var StateKeeper = {
-   buildsql: function() {}
-} */
-
-function Node() {
-   var commonNode = getNewNode();
-   for (var prop in commonNode) {
-      this.prop = commonNode[prop];
-   }
-}
-
-function Property(propertyname, displayname, propertyValues){
-   this.propertyValues = propertyValues;
-   this.propertyname = propertyname;
-   this.displayname =  displayname;
-
-}
-
-function StringProperty(propertyname, displayname, propertyValues){
-   this.propertyValues = propertyValues;
-   this.name = propertyname;
-   this.displayname =  displayname;
-
-}
-StringProperty.prototype = new Property();
-
-function NumericProperty(){
-
-}
-/*
-function getNewNode(foo, bar) {
-   var args = Array.prototype.slice.call(arguments);
-   return {
-      foo: args[1]
-   }
-}
-
-typeof getNewNode().foo === 'undefined' */
-
-
-function NounNode(name, displayname){
-   this.name = name;
-   this.displayname = displayname;
-   this.properties = [];
-   this.edges = [];
-
-
-}
-NounNode.prototype = new Node();
-
-function VerbNode(name,displayname){
-   this.name = name;
-   this.displayname = displayname;
-   this.properties =[];
-   this.edges = [];
-
-}
-
-VerbNode.prototype = new Node();
-
-function Node() {
-  //this.edges = [];
-  this.getAllTransitions = function(){
-      return this.edges.concat(this.properties);
-  };
-
-}
-
-function Edge(from, to, displayname){
-   this.fromNode = from;
-   this.toNode = to;
-   this.displayname = displayname;
-
-}
-
-//Testing the api: Next step generate this from a preferences files/autogenerate from create Schema
-drinkerNames = ["Ben", "James", "Jesse"];
-drinkerAddress = ["Central Campus, West Campus, West Campus"];
-
-barNames = ["Satisifactions", "Shooters"];
-
-beerNames = ["Corona", "Bud Light", "Blue Moon"];
-
-var drinkerNameProp = new StringProperty("name", "named", drinkerNames);
-var drinkerAddressProp = new StringProperty("address", "who live", drinkerAddress);
-
-var barNameProp = new StringProperty("bar_name", "named", barNames);
-var beerNameProp = new StringProperty("beer_name", "named", beerNames);
-
-
-
-
-drinkerNode = new  NounNode("Drinker", "Drinkers");
-drinkerNode.properties.push(drinkerNameProp);
-drinkerNode.properties.push(drinkerAddressProp);
-
-barNode = new NounNode("Bar", "Bars");
-barNode.properties.push(barNameProp);
-
-beerNode =  new NounNode("Beer", "Beers");
-beerNode.properties.push(beerNameProp);
-
-likesNode = new VerbNode("Like", "Likes");
-frequentsNode = new VerbNode("Frequents", "Frequents");
-servesNode = new VerbNode("Serves", "Serves");
-
-barToFrequents = new Edge(barNode, frequentsNode, "with frequenters");
-frequentsToBar = new Edge(frequentsNode, barNode, "bars");
-
-barToServes = new Edge(barNode, servesNode, "that serves ");
-servesToBar = new Edge(servesNode, barNode,  "bars");
-
-drinkerToLikes = new Edge(drinkerNode, likesNode, "who likes");
-likesToDrinker = new Edge(likesNode, drinkerNode, "drinkers");
-
-drinkersToFrequents = new Edge(drinkerNode, frequentsNode, "who frequent");
-frequentsToDrinkers =  new Edge(frequentsNode, drinkerNode, "who"); //nameless node that just opens up other options
-
-beerToLikes = new Edge(beerNode, likesNode, "liked by");
-likesToBeer = new Edge(likesNode, beerNode, "beers");
-
-beerToServes = new Edge(beerNode, servesNode, "served by");
-servesToBeer = new Edge(servesNode, beerNode, "beers");
-
-drinkerNode.edges.push(drinkerToLikes);
-drinkerNode.edges.push(drinkersToFrequents);
-
-barNode.edges.push(barToFrequents);
-barNode.edges.push(barToServes);
-
-beerNode.edges.push(beerToLikes);
-beerNode.edges.push(beerToServes);
-
-likesNode.edges.push(likesToBeer);
-likesNode.edges.push(likesToDrinker);
-
-servesNode.edges.push(servesToBar);
-servesNode.edges.push(servesToBeer);
-
-frequentsNode.edges.push(frequentsToDrinkers);
-frequentsNode.edges.push(frequentsToBar);
-
-
-
-
-var nouns = [drinkerNode, barNode, beerNode];
-var verbs = [likesNode, frequentsNode, servesNode];
-
-stateKeeper = new StateKeeper(nouns, verbs);
-
-setPromptSet(stateKeeper.buildValidPhrases());
-
-
